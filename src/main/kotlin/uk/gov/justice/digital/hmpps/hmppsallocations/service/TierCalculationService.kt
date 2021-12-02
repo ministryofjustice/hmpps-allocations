9f7920589d5c0be6d11e29d519afcf50bcfb1e3f
Mark Rees <mark.rees@digital.justice.gov.uk>
2021-12-02 09:29:03 +0000

    WFP-639 use transactional annotation instead of save

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/TierCalculationService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/TierCalculationService.kt
@@ -1,9 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsallocations.service
 
-import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.HmppsTierApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.jpa.repository.UnallocatedCasesRepository
+import javax.transaction.Transactional
 
 @Service
 class TierCalculationService(
@@ -11,16 +11,11 @@ class TierCalculationService(
   private val repository: UnallocatedCasesRepository
 ) {
 
+  @Transactional
   fun updateTier(crn: String) {
     repository.findByCrn(crn).ifPresent {
       val tier = hmppsTierApiClient.getTierByCrn(crn)
       it.tier = tier
-      repository.save(it)
-      log.info("Tier updated for CRN: $crn")
     }
-  }
-
-  companion object {
-    private val log = LoggerFactory.getLogger(this::class.java)
   }
 }
