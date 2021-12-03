6456a59b27fc0d0d98c096525e96daed965211a4
trozzomoj <angelo.trozzo@digital.justice.gov.uk>
2021-12-03 15:02:45 +0000

    added extra logging for unallocated case event listener.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/listener/EventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/listener/EventListener.kt
@@ -18,18 +18,16 @@ class EventListener(
 ) {
 
   @JmsListener(destination = "hmppsdomainqueue", containerFactory = "hmppsQueueContainerFactoryProxy")
-  fun processMessage(rawMessage: String?) {
-
-    val (message) = objectMapper.readValue(rawMessage, Message::class.java)
-    val event = objectMapper.readValue(message, HmppsEvent::class.java)
-
-    val crn = event.additionalInformation.crn
+  fun processMessage(rawMessage: String) {
+    val crn = getCrn(rawMessage)
     log.info("received event for crn: {}", crn)
+
     val sentenceDate = unallocatedCasesService.getSentenceDate(crn)
     val initialAppointment = unallocatedCasesService.getInitialAppointmentDate(crn, sentenceDate)
     val name = unallocatedCasesService.getOffenderName(crn)
     val tier = unallocatedCasesService.getTier(crn)
     val (status, previousConvictionDate) = unallocatedCasesService.getProbationStatus(crn)
+
     val unallocatedCase = UnallocatedCaseEntity(
       null, name,
       crn, tier, sentenceDate, initialAppointment, status.status, previousConvictionDate
@@ -40,8 +38,14 @@ class EventListener(
 
   @MessageExceptionHandler()
   fun errorHandler(e: Exception, msg: String) {
-    log.warn("Failed to to process Message")
+    log.warn("Failed to create an unallocated case  with CRN ${getCrn(msg)} with error: ${e.message}")
     throw e
+  }
+
+  private fun getCrn(rawMessage: String): String {
+    val (message) = objectMapper.readValue(rawMessage, Message::class.java)
+    val event = objectMapper.readValue(message, HmppsEvent::class.java)
+    return event.additionalInformation.crn
   }
 
   companion object {
