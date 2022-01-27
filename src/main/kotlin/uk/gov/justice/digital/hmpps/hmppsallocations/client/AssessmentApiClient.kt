4810f7cd73f56c91d61cf2ea22cf97f4f9d69246
carlov20 <carlov20@users.noreply.github.com>
2022-01-27 14:37:31 +0000

    Wfp 787 (#34)
    * WFP-787 passed through user token for ARN interactions
    
    * WFP-787 fixed failing tests
    
    * WFP-787 error scenarios for missing data

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/AssessmentApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/AssessmentApiClient.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.hmppsallocations.client
 
+import org.springframework.http.HttpStatus
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppsallocations.domain.Assessment
@@ -11,6 +12,18 @@ class AssessmentApiClient(private val webClient: WebClient) {
       .get()
       .uri("/needs/crn/$crn")
       .retrieve()
+      .onStatus(
+        { httpStatus -> HttpStatus.NOT_FOUND == httpStatus },
+        { Mono.error(MissingAssessmentError("No assessment found for $crn")) }
+      )
       .bodyToMono(Assessment::class.java)
+      .onErrorResume { ex ->
+        when (ex) {
+          is MissingAssessmentError -> Mono.just(Assessment(null))
+          else -> Mono.error(ex)
+        }
+      }
   }
 }
+
+private class MissingAssessmentError(msg: String) : RuntimeException(msg)
