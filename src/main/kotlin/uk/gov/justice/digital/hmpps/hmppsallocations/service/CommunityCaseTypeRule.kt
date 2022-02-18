d1004efd189f0a18d8e85de122bb6ff8f3a8045f
carlov20 <carlov20@users.noreply.github.com>
2022-02-18 13:26:28 +0000

    WFP-915 added no custody scenario

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/CommunityCaseTypeRule.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/CommunityCaseTypeRule.kt
@@ -7,7 +7,7 @@ class CommunityCaseTypeRule : CaseTypeRule {
 
   private val communitySentenceCodes = setOf("SP", "NP")
 
-  override fun isCaseType(sentenceTypeCode: String, custodialStatusCode: String): Boolean = communitySentenceCodes.contains(sentenceTypeCode)
+  override fun isCaseType(sentenceTypeCode: String, custodialStatusCode: String?): Boolean = communitySentenceCodes.contains(sentenceTypeCode)
 
   override fun getCaseType(): String = "COMMUNITY"
 }
