410c730878053b862a9e5bec4c1795e9d03cef58
Katie Cawthorne <51707463+katie-cawthorne@users.noreply.github.com>
2022-03-02 08:58:16 +0000

    :sparkles: wfp-920: add ogrs last updated (#61)
    * :sparkles: wfp-920: add ogrs last updated
    
    * :sparkles: wfp-920: remove comma to fix tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/OffenderAssessment.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/OffenderAssessment.kt
@@ -2,7 +2,9 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.domain
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import java.math.BigInteger
+import java.time.LocalDate
 
 data class OffenderAssessment @JsonCreator constructor(
+  val ogrsLastUpdate: LocalDate?,
   val ogrsScore: BigInteger?
 )
