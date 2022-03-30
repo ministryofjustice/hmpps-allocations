8ef9cc6fbc853e208ef8280fc2692a125de110a2
carlov20 <carlov20@users.noreply.github.com>
2022-03-30 15:28:44 +0100

    WFP-985 removed unused endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/WorkloadApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/WorkloadApiClient.kt
@@ -1,26 +1,10 @@
 package uk.gov.justice.digital.hmpps.hmppsallocations.client
 
-import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.CaseTypes
 import uk.gov.justice.digital.hmpps.hmppsallocations.domain.OffenderManagerOverview
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.PotentialCaseRequest
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.PotentialOffenderManagerWorkload
 
 class WorkloadApiClient(private val webClient: WebClient) {
-
-  fun getPotentialCaseLoad(tier: String, caseType: CaseTypes, offenderManagerCode: String): Mono<PotentialOffenderManagerWorkload> {
-    return webClient
-      .post()
-      .uri("/team/N03F01/offenderManagers/$offenderManagerCode/potentialCases")
-      .bodyValue(PotentialCaseRequest(tier, caseType))
-      .headers {
-        it.contentType = MediaType.APPLICATION_JSON
-      }
-      .retrieve()
-      .bodyToMono(PotentialOffenderManagerWorkload::class.java)
-  }
 
   fun getOffenderManagerOverview(offenderManagerCode: String): Mono<OffenderManagerOverview> {
     return webClient
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/GetAllocateCaseService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/GetAllocateCaseService.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.service
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.WorkloadApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.domain.OfficerOverviewAllocateCase
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.PotentialAllocateCase
 import uk.gov.justice.digital.hmpps.hmppsallocations.jpa.repository.UnallocatedCasesRepository
 import uk.gov.justice.digital.hmpps.hmppsallocations.mapper.GradeMapper
 
@@ -13,15 +12,6 @@ class GetAllocateCaseService(
   private val workloadApiClient: WorkloadApiClient,
   private val gradeMapper: GradeMapper
 ) {
-
-  fun getImpactOfAllocation(crn: String, convictionId: Long, offenderManagerCode: String): PotentialAllocateCase? =
-    unallocatedCasesRepository.findCaseByCrnAndConvictionId(crn, convictionId)?.let {
-      return workloadApiClient.getPotentialCaseLoad(it.tier, it.caseType, offenderManagerCode)
-        .map { offenderManagerWorkload ->
-          PotentialAllocateCase.from(it, offenderManagerWorkload, gradeMapper.workloadToStaffGrade(offenderManagerWorkload.grade))
-        }
-        .block()
-    }
 
   fun getOfficerOverview(crn: String, convictionId: Long, offenderManagerCode: String): OfficerOverviewAllocateCase? =
     unallocatedCasesRepository.findCaseByCrnAndConvictionId(crn, convictionId)?.let {
