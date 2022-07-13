300f764b3edbc0b460dd47befd3831bc38afd998
carlov20 <carlov20@users.noreply.github.com>
2022-07-13 09:16:49 +0100

    RUNFIX: handle when CRN is deleted in Delius

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/integration/IntegrationTestBase.kt
@@ -299,6 +299,15 @@ abstract class IntegrationTestBase {
     )
   }
 
+  protected fun notFoundAllConvictionResponse(crn: String) {
+    val convictionsRequest =
+      request().withPath("/offenders/crn/$crn/convictions")
+
+    communityApi.`when`(convictionsRequest, exactly(1)).respond(
+      response().withContentType(APPLICATION_JSON).withStatusCode(HttpStatus.NOT_FOUND.value())
+    )
+  }
+
   protected fun singleActiveRequirementResponse(crn: String, convictionId: Long) {
     val convictionsRequest =
       request().withPath("/offenders/crn/$crn/convictions/$convictionId/requirements").withQueryStringParameter(Parameter("activeOnly", "true"))
