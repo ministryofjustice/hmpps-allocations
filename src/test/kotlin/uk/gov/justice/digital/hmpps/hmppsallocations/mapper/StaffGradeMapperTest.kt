2108bfa2dcbe2e34da6d6addc4550ec5d87b5eba
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-07-15 12:07:12 +0100

    WFP-1132 rename test

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/mapper/StaffGradeMapperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/mapper/StaffGradeMapperTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.params.provider.Arguments.arguments
 import org.junit.jupiter.params.provider.MethodSource
 import java.util.stream.Stream
 
-class DefaultGradeMapperTest {
+class StaffGradeMapperTest {
 
   @ParameterizedTest
   @MethodSource("deliusGradeMap")
