3749328f953784e1317f8534014ae4a60052ec7a
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-07-15 12:06:08 +0100

    WFP-1132 simplify function

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/mapper/StaffGradeMapper.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/mapper/StaffGradeMapper.kt
@@ -2,6 +2,4 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.mapper
 
 private val gradeMap: Map<String, String> = mapOf("PSQ" to "PSO", "PSP" to "PQiP", "PSM" to "PO", "PSC" to "SPO")
 
-fun deliusToStaffGrade(deliusCode: String?): String? {
-  return gradeMap[deliusCode]
-}
+fun deliusToStaffGrade(deliusCode: String?): String? = gradeMap[deliusCode]
