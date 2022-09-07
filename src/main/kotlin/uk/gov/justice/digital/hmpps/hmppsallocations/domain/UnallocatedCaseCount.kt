85a4d705cd1be5a71a0bf49da25efb8343d95fcd
carlov20 <carlov20@users.noreply.github.com>
2022-09-07 10:46:05 +0100

    WFP-1354 removed unused endpoint

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/UnallocatedCaseCount.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsallocations.domain
-
-import com.fasterxml.jackson.annotation.JsonCreator
-import io.swagger.v3.oas.annotations.media.Schema
-
-data class UnallocatedCaseCount @JsonCreator constructor(
-  @Schema(description = "Count", example = "6")
-  val count: Long
-)
