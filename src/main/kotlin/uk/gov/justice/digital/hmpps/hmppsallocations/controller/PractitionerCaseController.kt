76d69622a91f0d9cce825d0bbaa8caf3a8366535
AndySweilam <AndySweilam@users.noreply.github.com>
2022-10-13 15:06:15 +0100

    WFP-1416:optimise imports.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/controller/PractitionerCaseController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/controller/PractitionerCaseController.kt
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.hmppsallocations.client.CommunityApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.domain.OffenderManagerDetails
 import uk.gov.justice.digital.hmpps.hmppsallocations.service.GetUnallocatedCaseService
 import uk.gov.justice.digital.hmpps.hmppsallocations.service.exception.EntityNotFoundException
