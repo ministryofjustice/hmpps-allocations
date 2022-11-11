f1215de28b71f61ee235a030871793242a53b139
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-11-11 09:31:12 +0000

    FIXBUILD: upgrade all swagger dependencies at once

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,9 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:1.1.13")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.12")
   implementation("org.springdoc:springdoc-openapi-kotlin:1.6.12")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.12")
 
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
