cf8ca7b288f0477a88ff4ef8851162663254f4d5
sp-watson <steve.watson@digital,justice.gov.uk>
2022-12-15 11:49:16 +0000

    WFP-1648: Use dedicated data structure for overview endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/CaseOverview.kt
@@ -0,0 +1,52 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.domain
+
+import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonFormat
+import com.fasterxml.jackson.annotation.JsonInclude
+import io.swagger.v3.oas.annotations.media.Schema
+import uk.gov.justice.digital.hmpps.hmppsallocations.jpa.entity.UnallocatedCaseEntity
+import java.time.LocalDate
+
+data class CaseOverview @JsonCreator constructor(
+  @Schema(description = "Offender Name", example = "John Smith")
+  val name: String,
+  @Schema(description = "CRN", example = "J111111")
+  val crn: String,
+  @Schema(description = "Latest tier of case", example = "D2")
+  val tier: String,
+  @Schema(description = "Sentence Date", example = "2020-01-16")
+  @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+  val sentenceDate: LocalDate,
+  @Schema(description = "Initial Appointment Date", example = "2020-03-21")
+  @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
+  val initialAppointment: LocalDate?,
+  @Schema(description = "Probation Status", example = "Currently managed")
+  val status: String,
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  val offenderManager: OffenderManagerDetails?,
+  @Schema(description = "Conviction Id")
+  val convictionId: Long,
+  @Schema(description = "Case Type")
+  val caseType: CaseTypes,
+  @Schema(description = "Sentence Length")
+  val sentenceLength: String?,
+  val convictionNumber: Int
+) {
+  companion object {
+    fun from(case: UnallocatedCaseEntity): CaseOverview {
+      return CaseOverview(
+        case.name,
+        case.crn, case.tier, case.sentenceDate, case.initialAppointment, case.status,
+        OffenderManagerDetails(
+          case.offenderManagerForename,
+          case.offenderManagerSurname,
+          case.offenderManagerGrade
+        ),
+        case.convictionId,
+        case.caseType,
+        case.sentenceLength,
+        case.convictionNumber
+      )
+    }
+  }
+}
