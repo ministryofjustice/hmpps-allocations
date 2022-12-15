43ad2791b0f65055a6e7c8ce073ddd7055ce303e
Mark Rees <mark.rees@digital.justice.gov.uk>
2022-12-15 12:07:23 +0000

    WFP-1648 remove unused fields from caseOverview

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/CaseOverview.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/domain/CaseOverview.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.domain
 
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonFormat
-import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.v3.oas.annotations.media.Schema
 import uk.gov.justice.digital.hmpps.hmppsallocations.jpa.entity.UnallocatedCaseEntity
 import java.time.LocalDate
@@ -14,37 +13,23 @@ data class CaseOverview @JsonCreator constructor(
   val crn: String,
   @Schema(description = "Latest tier of case", example = "D2")
   val tier: String,
-  @Schema(description = "Sentence Date", example = "2020-01-16")
-  @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
-  val sentenceDate: LocalDate,
   @Schema(description = "Initial Appointment Date", example = "2020-03-21")
   @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
   val initialAppointment: LocalDate?,
-  @Schema(description = "Probation Status", example = "Currently managed")
-  val status: String,
-  @JsonInclude(JsonInclude.Include.NON_NULL)
-  val offenderManager: OffenderManagerDetails?,
   @Schema(description = "Conviction Id")
   val convictionId: Long,
   @Schema(description = "Case Type")
   val caseType: CaseTypes,
-  @Schema(description = "Sentence Length")
-  val sentenceLength: String?,
+  @Schema(description = "Conviction Number")
   val convictionNumber: Int
 ) {
   companion object {
     fun from(case: UnallocatedCaseEntity): CaseOverview {
       return CaseOverview(
         case.name,
-        case.crn, case.tier, case.sentenceDate, case.initialAppointment, case.status,
-        OffenderManagerDetails(
-          case.offenderManagerForename,
-          case.offenderManagerSurname,
-          case.offenderManagerGrade
-        ),
+        case.crn, case.tier, case.initialAppointment,
         case.convictionId,
         case.caseType,
-        case.sentenceLength,
         case.convictionNumber
       )
     }
