028f5f4a677af8d81b071e62d9b9279de51d4e08
sp-watson <77974320+sp-watson@users.noreply.github.com>
2022-12-20 11:53:07 +0000

    WFP-1720: Log when events delivery delayed by >1 second (#274)
    * WFP-1720: Log >1s delays to event delivery
    
    * Fix int test
    
    Co-authored-by: sp-watson <steve.watson@digital,justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/listener/CalculationEventListener.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/listener/CalculationEventListener.kt
@@ -2,9 +2,12 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.listener
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
 import org.springframework.jms.annotation.JmsListener
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.hmppsallocations.service.TierCalculationService
+import java.time.Duration
+import java.time.LocalDateTime
 
 @Component
 class CalculationEventListener(
@@ -20,13 +23,23 @@ class CalculationEventListener(
 
   private fun readMessage(wrapper: String?): CalculationEventData {
     val (message) = objectMapper.readValue(wrapper, Message::class.java)
-    return objectMapper.readValue(message, CalculationEventData::class.java)
+    val eventData = objectMapper.readValue(message, CalculationEventData::class.java)
+    eventData.occurredAt.let {
+      val timeToDeliverMs = Duration.between(it, LocalDateTime.now())
+      if (timeToDeliverMs.seconds > 1000) {
+        log.warn("Delayed TierCalculationEvent delivery (${timeToDeliverMs.seconds}): Crn ${crnFrom(eventData)}")
+      }
+    }
+    return eventData
   }
 
   private fun crnFrom(calculationEventData: CalculationEventData) =
     calculationEventData.personReference.identifiers.first { it.type == "CRN" }.value
 
-  data class CalculationEventData(val personReference: PersonReference)
+  data class CalculationEventData(
+    val personReference: PersonReference,
+    val occurredAt: LocalDateTime
+  )
 
   data class PersonReference(val identifiers: List<PersonReferenceType>)
 
@@ -36,4 +49,8 @@ class CalculationEventListener(
   )
 
   data class Message(@JsonProperty("Message") val message: String)
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
 }
