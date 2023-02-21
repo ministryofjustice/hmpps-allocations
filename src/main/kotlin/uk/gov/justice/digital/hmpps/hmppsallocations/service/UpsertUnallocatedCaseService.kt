9fcd12f887b69d95bb73c73410cb856378e2a26b
carlov20 <carlov20@users.noreply.github.com>
2023-02-21 15:48:17 +0000

    WFP-1809 refactor

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/UpsertUnallocatedCaseService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/UpsertUnallocatedCaseService.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.service
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.HmppsTierApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.WorkforceAllocationsToDeliusApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.ActiveEvent
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.UnallocatedEvents
@@ -13,7 +14,7 @@ import javax.transaction.Transactional
 @Service
 class UpsertUnallocatedCaseService(
   private val repository: UnallocatedCasesRepository,
-  private val enrichEventService: EnrichEventService,
+  @Qualifier("hmppsTierApiClient") private val hmppsTierApiClient: HmppsTierApiClient,
   private val telemetryService: TelemetryService,
   @Qualifier("workforceAllocationsToDeliusApiClient") private val workforceAllocationsToDeliusApiClient: WorkforceAllocationsToDeliusApiClient,
 
@@ -24,7 +25,7 @@ class UpsertUnallocatedCaseService(
     val storedUnallocatedEvents = repository.findByCrn(crn)
     workforceAllocationsToDeliusApiClient.getUnallocatedEvents(crn).block()?.let { unallocatedEvents ->
       val activeEvents = unallocatedEvents.activeEvents.associateBy { it.eventNumber.toInt() }
-      enrichEventService.getTier(crn)?.let { tier ->
+      hmppsTierApiClient.getTierByCrn(crn)?.let { tier ->
 
         saveNewEvents(activeEvents, storedUnallocatedEvents, unallocatedEvents, tier)
         updateExistingEvents(activeEvents, storedUnallocatedEvents, unallocatedEvents, tier)
