8ff8e7b5aebb795826dd8ecf4399093ee545ca78
carlov20 <carlov20@users.noreply.github.com>
2023-02-27 12:37:07 +0000

    WFP-1848 added upload crn endpoint

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/controller/TriggerProcessingCrnController.kt
@@ -0,0 +1,36 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.controller
+
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.reactive.asFlow
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.hmppsallocations.service.TriggerReprocessService
+
+@RestController
+@RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
+class TriggerProcessingCrnController(private val triggerReprocessService: TriggerReprocessService) {
+
+  @PostMapping("/crn/upload")
+  suspend fun uploadCrns(@RequestPart("file") file: Mono<FilePart>): ResponseEntity<Void> {
+    triggerReprocessService.sendEvents(fileToCases(file))
+    return ResponseEntity.ok().build()
+  }
+
+  suspend fun fileToCases(filePart: Mono<FilePart>): List<String> {
+    return filePart.flatMapMany { file ->
+      file.content().flatMapIterable { dataBuffer ->
+        dataBuffer.asInputStream().bufferedReader().use { reader ->
+          reader.lineSequence()
+            .filter { it.isNotBlank() }
+            .toList()
+        }
+      }
+    }.asFlow().toList()
+  }
+}
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/TriggerReprocessService.kt
@@ -0,0 +1,55 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.service
+
+import com.amazonaws.services.sqs.AmazonSQS
+import com.amazonaws.services.sqs.model.MessageAttributeValue
+import com.amazonaws.services.sqs.model.SendMessageRequest
+import com.amazonaws.services.sqs.model.SendMessageResult
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsallocations.listener.HmppsOffenderEvent
+import uk.gov.justice.digital.hmpps.hmppsallocations.listener.SQSMessage
+import uk.gov.justice.hmpps.sqs.HmppsQueueService
+import uk.gov.justice.hmpps.sqs.MissingQueueException
+
+@Service
+class TriggerReprocessService(
+  private val hmppsQueueService: HmppsQueueService,
+  private val objectMapper: ObjectMapper,
+  @Qualifier("hmppsoffenderqueue-sqs-client") private val hmppsOffenderSqsClient: AmazonSQS,
+) {
+
+  private val hmppsOffenderQueueUrl = hmppsQueueService.findByQueueId("hmppsoffenderqueue")?.queueUrl ?: throw MissingQueueException("HmppsQueue hmppsoffenderqueue not found")
+
+  @Async
+  fun sendEvents(crns: List<String>) {
+    crns.forEach { crn ->
+      publishToHMPPSOffenderQueue(crn)
+    }
+  }
+
+  private fun publishToHMPPSOffenderQueue(crn: String): SendMessageResult {
+    val sendMessage = SendMessageRequest(
+      hmppsOffenderQueueUrl,
+      objectMapper.writeValueAsString(
+        crnToOffenderSqsMessage(crn)
+      )
+    ).withMessageAttributes(
+      mapOf("eventType" to MessageAttributeValue().withDataType("String").withStringValue("CONVICTION_CHANGED"))
+    )
+    log.info("publishing event type {} for crn {}", "CONVICTION_CHANGED", crn)
+    return hmppsOffenderSqsClient.sendMessage(sendMessage)
+  }
+
+  private fun crnToOffenderSqsMessage(crn: String): SQSMessage = SQSMessage(
+    objectMapper.writeValueAsString(
+      HmppsOffenderEvent(crn)
+    )
+  )
+
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+}
