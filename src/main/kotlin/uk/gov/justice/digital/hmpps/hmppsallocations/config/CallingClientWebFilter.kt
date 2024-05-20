f8a46142caf09b04b9c011f20b92564d1ea47c85
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-05-20 13:29:16 +0100

    WFP-2487-Adding in custom dimension for logging

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/config/CallingClientWebFilter.kt
@@ -0,0 +1,35 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.config
+
+import com.nimbusds.jwt.SignedJWT
+import io.opentelemetry.api.trace.Span
+import org.springframework.http.HttpHeaders
+import org.springframework.stereotype.Component
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+
+private const val BEARER = "Bearer "
+private const val CLIENT_ID = "client_id"
+
+@Component
+class CallingClientWebFilter : WebFilter {
+  override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+    val auth = exchange.request.headers.get(HttpHeaders.AUTHORIZATION)
+    val predicate: (String) -> Boolean = { it.startsWith(BEARER) }
+    val claims = auth?.first(predicate)?.substringAfter(BEARER)
+    if (claims != null) {
+      Span.current().setAttribute(
+        CLIENT_ID,
+        SignedJWT.parse(claims).jwtClaimsSet
+          .getClaim(CLIENT_ID).toString(),
+      )
+    } else {
+      Span.current().setAttribute(
+        CLIENT_ID,
+        "No Client id provided",
+      )
+    }
+    return chain.filter(exchange)
+  }
+}
