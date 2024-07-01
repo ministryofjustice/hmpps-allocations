3d3858927104e6f19d2106691b7c07a89ab60a31
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-07-01 14:39:32 +0100

    WFP-2556-poc-for-retries-adding-tests-lint

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/WorkforceAllocationsToDeliusApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/WorkforceAllocationsToDeliusApiClientTest.kt
@@ -9,13 +9,13 @@ import org.springframework.web.reactive.function.client.ExchangeFunction
 import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
-
 class WorkforceAllocationsToDeliusApiClientTest {
   @Test
-  fun `test 500 retries on delius client` () = runBlocking {
-    val exchangeFunction = ExchangeFunction{
-      request -> Mono.just(ClientResponse.create(HttpStatus.INTERNAL_SERVER_ERROR).build())
-  }
+  fun `test 500 retries on delius client`() = runBlocking {
+    val exchangeFunction = ExchangeFunction {
+        request ->
+      Mono.just(ClientResponse.create(HttpStatus.INTERNAL_SERVER_ERROR).build())
+    }
     val webClient = WebClient.builder().exchangeFunction(exchangeFunction).build()
     val exception = assertThrows<RuntimeException> {
       WorkforceAllocationsToDeliusApiClient(webClient).getUnallocatedEvents("999999")
@@ -24,9 +24,10 @@ class WorkforceAllocationsToDeliusApiClientTest {
   }
 
   @Test
-  fun `test 404 on delius client` () = runBlocking {
-    val exchangeFunction = ExchangeFunction{
-        request -> Mono.just(ClientResponse.create(HttpStatus.NOT_FOUND).build())
+  fun `test 404 on delius client`() = runBlocking {
+    val exchangeFunction = ExchangeFunction {
+        request ->
+      Mono.just(ClientResponse.create(HttpStatus.NOT_FOUND).build())
     }
     val webClient = WebClient.builder().exchangeFunction(exchangeFunction).build()
     val result = WorkforceAllocationsToDeliusApiClient(webClient).getUnallocatedEvents("999999")
@@ -34,12 +35,12 @@ class WorkforceAllocationsToDeliusApiClientTest {
   }
 
   @Test
-  fun `test 403 on delius client` () = runBlocking {
-    val exchangeFunction = ExchangeFunction{
-        request -> Mono.just(ClientResponse.create(HttpStatus.FORBIDDEN).build())
+  fun `test 403 on delius client`() = runBlocking {
+    val exchangeFunction = ExchangeFunction {
+        request ->
+      Mono.just(ClientResponse.create(HttpStatus.FORBIDDEN).build())
     }
     val webClient = WebClient.builder().exchangeFunction(exchangeFunction).build()
-
     val exception = assertThrows<RuntimeException> {
       WorkforceAllocationsToDeliusApiClient(webClient).getUnallocatedEvents("999999")
     }
