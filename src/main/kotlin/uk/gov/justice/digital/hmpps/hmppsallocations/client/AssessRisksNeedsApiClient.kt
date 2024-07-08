d65a259ecceb7b4adf863b6ff3002fde9e9f5081
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2024-07-08 15:24:39 +0100

    WFP-2608 testing epic

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/AssessRisksNeedsApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/AssessRisksNeedsApiClient.kt
@@ -22,6 +22,7 @@ import java.math.BigDecimal
 
 class AssessRisksNeedsApiClient(private val webClient: WebClient) {
   // test comment WFP-2608
+  // another test comment
 
   suspend fun getLatestCompleteAssessment(crn: String): Assessment? {
     return webClient
