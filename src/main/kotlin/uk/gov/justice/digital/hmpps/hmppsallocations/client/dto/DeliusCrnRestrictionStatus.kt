6069c27928453d2e9b0a204c1a7b050cbf1986c6
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2024-12-13 14:38:26 +0000

    Revert "WFP-2896 new lao endpoint (#558)" (#559)
    This reverts commit d29e06b37da17ffa15308731cf790861c45a0de5.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/dto/DeliusCrnRestrictionStatus.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.hmppsallocations.client.dto
-
-data class DeliusCrnRestrictionStatus(
-  val crn: String,
-  val isRestricted: Boolean,
-  val isRedacted: Boolean,
-)
