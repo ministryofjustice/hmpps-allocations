0c44c9261f7ef39071188d88d728acc710c279b7
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-14 15:59:52 +0100

    update spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.1.2"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.0.0"
   kotlin("plugin.spring") version "2.1.0"
   kotlin("plugin.jpa") version "2.1.0"
   id("io.gitlab.arturbosch.detekt") version "1.23.7"
@@ -40,7 +40,7 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.1")
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.2")
 
-  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.5")
+  implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.6")
 
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
