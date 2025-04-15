a117a5aba534b03fb766cbad7f8862e056deb952
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-04-15 18:22:57 +0100

    first commit - need tests

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/controller/RegionAccessController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/controller/RegionAccessController.kt
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.RegionList
 import uk.gov.justice.digital.hmpps.hmppsallocations.service.GetRegionsService
+import uk.gov.justice.digital.hmpps.hmppsallocations.service.ValidateAccessService
+import uk.gov.justice.digital.hmpps.hmppsallocations.service.exception.NotAllowedForAccessException
 
 @RestController
 @RequestMapping(produces = [MediaType.APPLICATION_JSON_VALUE])
-class RegionAccessController(private val getRegionsService: GetRegionsService) {
+class RegionAccessController(private val getRegionsService: GetRegionsService,
+                             private val validateAccessService: ValidateAccessService) {
 
   @Operation(summary = "Retrieve all user regions")
   @ApiResponses(
@@ -25,4 +29,22 @@ class RegionAccessController(private val getRegionsService: GetRegionsService) {
   @PreAuthorize("hasRole('ROLE_MANAGE_A_WORKFORCE_ALLOCATE')")
   @GetMapping("/user/{staffId}/regions")
   suspend fun getAccessibleRegionsByUser(@PathVariable staffId: String): RegionList = getRegionsService.getRegionsByUser(staffId)
+
+  @Operation(summary = "Check for user access")
+  @ApiResponses(
+    value = [
+      ApiResponse(responseCode = "200", description = "OK"),
+      ApiResponse(responseCode = "403", description = "Forbidden"),
+    ],
+  )
+  @PreAuthorize("hasRole('ROLE_MANAGE_A_WORKFORCE_ALLOCATE')")
+  @GetMapping("/user/{staffId}/crn/{crn}/is-allowed")
+  suspend fun getValidatedAccess(@PathVariable staffId: String, @PathVariable crn: String): ResponseEntity<Void> {
+    return try {
+      validateAccessService.validateUserAccess(staffId, crn)
+      ResponseEntity.ok().build()
+    } catch (e: NotAllowedForAccessException) {
+      ResponseEntity.status(403).build()
+    }
+  }
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/ValidateAccessService.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.service
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.HmppsProbationEstateApiClient
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.WorkforceAllocationsToDeliusApiClient
+import uk.gov.justice.digital.hmpps.hmppsallocations.service.exception.NotAllowedForAccessException
+
+@Component
+class ValidateAccessService(
+  @Qualifier("workforceAllocationsToDeliusApiClientUserEnhanced")
+  private val workforceAllocationsToDeliusApiClient: WorkforceAllocationsToDeliusApiClient,
+  private val hmppsProbationEstateApiClient: HmppsProbationEstateApiClient,
+  private val getRegionsService: GetRegionsService,
+)  {
+  suspend fun validateUserAccess(crn: String, staffCode: String): Boolean {
+    val allowedRegions = getRegionsService.getRegionsByUser(staffCode)
+    val probationEstateForPoP = workforceAllocationsToDeliusApiClient.getUnallocatedEvents(crn)?.activeEvents?.map { it.teamCode }
+    val probationEstateRegions =
+      hmppsProbationEstateApiClient.getRegionsAndTeams(setOf(probationEstateForPoP ?: emptyList()).flatten().toSet())
+        ?.map { it.region}
+    val result = allowedRegions.regions.intersect(probationEstateRegions?.toSet() ?: emptySet()).size > 0
+    if (!result) {
+      throw NotAllowedForAccessException("User does not have access to crn:", crn)
+    }
+    return true
+  }
+}
