26215e9413f3bcfa069e213930a4e790f53b8b6e
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-05-29 15:18:48 +0100

    Wfp 3057 new access endpoint check crn (#603)
    * first commit - need tests
    
    * linting
    
    * test and little tweak
    
    * Tweak to controller and exception message
    
    * Tweak to add in conviction number
    
    * simplified logic
    
    * swapping order of arguments
    
    * tightening up tests
    
    * changing response to Void
    
    * wfp-3057 - fix
    
    * wfp-3057 - tweak to regions service

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/dto/DeliusTeams.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/dto/DeliusTeams.kt
@@ -1,5 +1,11 @@
 package uk.gov.justice.digital.hmpps.hmppsallocations.client.dto
 
 data class DeliusTeams constructor(
+  val datasets: List<Dataset>,
   val teams: List<TeamWithLau>,
+)
+
+data class Dataset(
+  val code: String,
+  val description: String,
 )
