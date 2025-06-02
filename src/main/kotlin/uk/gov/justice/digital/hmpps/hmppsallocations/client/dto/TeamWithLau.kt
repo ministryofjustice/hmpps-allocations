a8ac6eaebc5f7c9db37bad563382aff4403881f2
sam.bailey <sam.bailey1@digital.justice.gov.uk>
2025-06-02 17:16:04 +0100

    linting

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/dto/TeamWithLau.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/client/dto/TeamWithLau.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.hmppsallocations.client.dto
 data class TeamWithLau constructor(
   val code: String = "",
   val description: String = "",
-  val localAdminUnit: Lau = Lau("","", Pdu("", "", Provider("", ""))),
+  val localAdminUnit: Lau = Lau("", "", Pdu("", "", Provider("", ""))),
 )
