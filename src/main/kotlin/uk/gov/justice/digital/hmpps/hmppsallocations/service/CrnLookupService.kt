8a434f9bacc83d141ba090db2cb5fcbd1aa2a9e5
Gary Reid <159784838+GaReid@users.noreply.github.com>
2025-10-14 10:00:34 +0100

    Wfp 3376 endpoint for crn lookup (#660)
    * WFP-3376 create BE endpoint for CRN Lookup initial commit
    
    * WFP-3376 create BE endpoint for CRN Lookup initial commit

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/CrnLookupService.kt
@@ -0,0 +1,14 @@
+package uk.gov.justice.digital.hmpps.hmppsallocations.service
+
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.WorkforceAllocationsToDeliusApiClient
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.CrnDetails
+
+@Service
+class CrnLookupService(
+  @Qualifier("workforceAllocationsToDeliusApiClientUserEnhanced") private val workforceAllocationsToDeliusApiClient: WorkforceAllocationsToDeliusApiClient,
+) {
+
+  suspend fun getCrnDetails(crn: String): CrnDetails = workforceAllocationsToDeliusApiClient.getCrnDetails(crn)
+}
