2957d75ee56fda24bf7150bb417c886bf0c821f7
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-11-06 11:28:23 +0000

    Wfp 3396 new endpoint for overview refactor (#669)
    * wfp-3396 first commit
    
    * wfp-3396 linting
    
    * wfp-3396 removing duplicate classes unneeded
    
    * wfp-3396 typo
    
    * wfp-3396 adding test
    
    * refactor and handle null pnc# or main address

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/GetAllocatedCaseServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsallocations/service/GetAllocatedCaseServiceTest.kt
@@ -9,13 +9,13 @@ import reactor.core.publisher.Mono
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.HmppsTierApiClient
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.Name
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.WorkforceAllocationsToDeliusApiClient
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.AllocatedActiveEvent
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.AllocatedEventOffences
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.AllocatedEventRequirement
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.AllocatedEventSentence
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.DeliusAllocatedCaseView
 import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.DeliusCrnRestrictionStatus
-import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.MainAddress
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.AllocatedActiveEvent
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.AllocatedEventOffences
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.AllocatedEventRequirement
-import uk.gov.justice.digital.hmpps.hmppsallocations.domain.AllocatedEventSentence
+import uk.gov.justice.digital.hmpps.hmppsallocations.client.dto.MainAddressDto
 import java.time.LocalDate
 
 class GetAllocatedCaseServiceTest {
@@ -43,10 +43,61 @@ class GetAllocatedCaseServiceTest {
       LocalDate.of(1980, 1, 1),
       "Male",
       "pnc123456",
-      MainAddress(
+      MainAddressDto(
         "The Manor", "1", "High Street", "London", "Greater London", "AB1 2CD", false, true, "home",
         LocalDate.of(2020, 1, 1),
       ),
+      nextAppointmentDate = LocalDate.of(2023, 12, 1),
+      activeEvents = listOf(
+        AllocatedActiveEvent(
+          1,
+          0,
+          LocalDate.of(2022, 1, 27),
+          AllocatedEventSentence(
+            "Custodial",
+            LocalDate.of(2022, 1, 15),
+            LocalDate.of(2023, 1, 15),
+            "12 months",
+          ),
+          listOf(AllocatedEventOffences("Burglary", "Theft", true)),
+          listOf(AllocatedEventRequirement("Curfew", "Must comply with curfew", "6 months")),
+        ),
+      ),
+    )
+
+    val deliusCrnRestrictionStatus = DeliusCrnRestrictionStatus(
+      crn = crn,
+      isRestricted = false,
+      isRedacted = false,
+    )
+
+    val tier = "C2"
+
+    // Arrange
+    coEvery { workforceAllocationsToDeliusApiClient.getAllocatedDeliusCaseView(any()) } returns Mono.just(deliusCaseView)
+    coEvery { laoService.getCrnRestrictionStatus(any(), any()) } returns deliusCrnRestrictionStatus
+    coEvery { tierApiClient.getTierByCrn(any()) } returns tier
+
+    // Act
+    val result = service.getCase("userName", crn)
+
+    // Assert
+    assert(result!!.tier == tier)
+    assert(result.crn == crn)
+    assert(result.activeEvents.size == 1)
+    assert(result.activeEvents.first().sentence!!.length == "12 months")
+    assert(result.activeEvents.first().offences.first().mainOffence == true)
+  }
+
+  @Test
+  fun `should return allocated case details with null address`() = runBlocking {
+    val crn = "X123456"
+    val deliusCaseView = DeliusAllocatedCaseView(
+      Name(forename = "John", middleName = null, surname = "Doe"),
+      LocalDate.of(1980, 1, 1),
+      "Male",
+      "pnc123456",
+      null,
       nextAppointmentDate = LocalDate.of(2023, 12, 1),
       activeEvents = listOf(
         AllocatedActiveEvent(
